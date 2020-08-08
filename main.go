9d9df77bda9996b984cf593e6312d116c5a9d3d1
Bartek Pacia <barpac02@gmail.com>
2020-08-08 18:15:21 +0200

    refactor to use Go idioms

--- main.go
+++ main.go
@@ -6,45 +6,42 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
-// Port is the server's address
-const Port = 5000
-// VerifyToken is the token used by Facebook to authorize the webhook
-var VerifyToken string
-// AccessToken is ...idk lol
-var AccessToken string
+var (
+	host string
+	port string
+	verifyToken string
+	accessToken string
+)
 
+func init()  {
+	flag.StringVar(&host, "host", "", "hostname to listen on")
+	flag.StringVar(&port, "port", "5000", "port to listen on")
 
-func main() {
 	godotenv.Load()
-	var env string
-	flag.StringVar(&env, "env", "dev", "env: dev | prod")
-	flag.Parse()
-	fmt.Println("env:", env)
-
-	addr := ":" + strconv.Itoa(Port)
-	if env == "dev" {
-		addr = "localhost:" + strconv.Itoa(Port)
-	}
-
 	var ok bool
-	VerifyToken, ok = os.LookupEnv("verify_token")
+	verifyToken, ok = os.LookupEnv("verify_token")
 	if !ok {
 		log.Fatalln("Error reading verify_token")
 	}
-	AccessToken, ok = os.LookupEnv("access_token")
+	accessToken, ok = os.LookupEnv("access_token")
 	if !ok {
 		log.Fatalln("Error reading access_token")
 	}
 
-	fmt.Println("access_token:", AccessToken)
+}
+
+func main() {
+	flag.Parse()
 
 	http.HandleFunc("/", HandleMain)
 	http.HandleFunc("/webhook", HandleWebhook)
+
+	addr := fmt.Sprintf("%s:%s", host, port)
+	fmt.Printf("Listening on %s...\n", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
@@ -56,14 +53,16 @@ func HandleMain(w http.ResponseWriter, r *http.Request) {
 // HandleWebhook handles "/webhook" route.
 func HandleWebhook(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
+		log.Printf("GET /webhook")
+
 		mode := r.URL.Query().Get("hub.mode")
 		token := r.URL.Query().Get("hub.verify_token")
 		challenge := r.URL.Query().Get("hub.challenge")
 
 		fmt.Printf("mode: %s, token: %s, challenge: %s\n", mode, token, challenge)
 		if mode != "" && token != "" {
-			if mode == "subscribe" && token == VerifyToken {
-				log.Println("Webhook successfully verified. 200")
+			if mode == "subscribe" && token == verifyToken {
+				log.Println("Webhook verified successfully. 200")
 				fmt.Fprintf(w, challenge)
 			} else {
 				log.Println("Webhook verification failed. 403")
@@ -76,6 +75,8 @@ func HandleWebhook(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == "POST" {
+		log.Printf("POST /webhook")
+		
 		w.WriteHeader(http.StatusNotImplemented)
 	}
 }
