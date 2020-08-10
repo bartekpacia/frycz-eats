cf76e3da177d788beb325b06ecf54ee6ba9b06a6
Bartek Pacia <barpac02@gmail.com>
2020-08-10 19:39:55 +0200

    cleanup

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	messenger "github.com/bartekpacia/facebook-messenger"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/bartekpacia/frycz-eats/messenger"
-	"github.com/bartekpacia/frycz-eats/webhooks"
 	"github.com/joho/godotenv"
+)
+
+const (
+	PhotoURL string = "https://firebasestorage.googleapis.com/v0/b/discoverrudy.appspot.com/o/static%2Frudy.jpg?alt=media"
 )
 
 var (
@@ -19,6 +21,7 @@ var (
 	port        string
 	verifyToken string
 	accessToken string
+	pageID      string
 )
 
 func init() {
@@ -26,23 +29,39 @@ func init() {
 	flag.StringVar(&port, "port", "5000", "port to listen on")
 
 	godotenv.Load()
+
 	var ok bool
 	verifyToken, ok = os.LookupEnv("verify_token")
 	if !ok {
 		log.Fatalln("verifyToken is empty")
 	}
+
 	accessToken, ok = os.LookupEnv("access_token")
 	if !ok {
 		log.Fatalln("accessToken is empty")
 	}
 
+	pageID, ok = os.LookupEnv("page_id")
+	if !ok {
+		log.Fatalln("pageID is empty")
+	}
 }
 
 func main() {
 	flag.Parse()
 
-	http.HandleFunc("/", HandleMain)
-	http.HandleFunc("/webhook", HandleWebhook)
+	bot := &messenger.Messenger{
+		AccessToken: accessToken,
+		VerifyToken: verifyToken,
+		PageID:      pageID,
+	}
+
+	bot.MessageReceived = messageReceived
+	bot.PostbackReceived = postbackReceived
+	// bot.DeliveryReceived = deliveryReceived // comment/delete if not used
+
+	http.HandleFunc("/", handleMain)
+	http.Handle("/bot", bot)
 
 	addr := fmt.Sprintf("%s:%s", host, port)
 	fmt.Printf("Listening on %s...\n", addr)
@@ -50,74 +69,51 @@ func main() {
 }
 
 // HandleMain handles "/" route.
-func HandleMain(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Frycz Eats API homepage. Hi!")
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprint(w, "Frycz Eats API homepage. Hi!")
+	if err != nil {
+		log.Println("Error writing text to homepage ResponseWriter:", err)
+	}
 }
 
-// HandleWebhook handles "/webhook" route.
-func HandleWebhook(w http.ResponseWriter, req *http.Request) {
-	if req.Method == "GET" {
-		log.Printf("GET /webhook")
-		webhooks.VerifyWebhook(w, req, verifyToken)
+func messageReceived(ctx context.Context, msng *messenger.Messenger, userID int64, m messenger.FacebookMessage) {
+	switch m.Text {
+	case "hi":
+		msng.SendTextMessage(userID, "Hello there")
+
+	case "postback":
+		gm := msng.NewGenericMessage(userID)
+		btn2 := msng.NewPostbackButton("Ok", "postback")
+		gm.AddNewElement("Title", "Subtitle", "https://google.pl", PhotoURL, []messenger.Button{btn2})
+
+		msng.SendMessage(gm)
+	case "button":
+		gm := msng.NewButtonMessage(userID, "message")
+
+		btnMakeOrder := msng.NewWebURLButton("title", "https://odkryjrudy.pl")
+		btnShowOrders := msng.NewWebURLButton("TITL", "https://odkryjrudy.pl")
+		gm.AddNewButton(btnMakeOrder)
+		gm.AddNewButton(btnShowOrders)
+
+		msng.SendMessage(gm)
+
+	default:
+		// upthere we haven't check for errors and responses for cleaner example code
+		// but keep in mind that SendMessage returns FacebookResponse struct and error
+		// errors are received from Facebook if sometnihg went wrong with message sending
+		resp, err := msng.SendTextMessage(userID, m.Text) // echo, send back to user the same text he sent to us
+		if err != nil {
+			log.Println(err)
+			return // if there is an error, resp is empty struct, useless
+		}
+		log.Println("Message ID", resp.MessageID, "sent to user", resp.RecipientID)
+		// store resp.MessageID if you want to track delivery reports that will be sent later from Facebook
 	}
+}
 
-	if req.Method == "POST" {
-		log.Println("POST /webhook")
-
-		bodyBytes, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			log.Println("Error reading request body", err)
-		}
-
-		entries, err := webhooks.UnmarshallEntries(bodyBytes)
-		if err != nil {
-			log.Fatalf("Error unmarshalling webhookEvents: %e\n", err)
-		}
-
-		for _, event := range entries {
-			webhookData := event.WebhookData[0]
-
-			if webhookData.Message != nil {
-				// responseText, err := webhookData.HandleMessage(accessToken)
-				// if err != nil {
-				// 	fmt.Println("Error handling message:", err)
-				// }
-
-				// user, err := messenger.GetUser(webhookData.Sender.ID, accessToken)
-				// if err != nil {
-				// 	fmt.Println("Error getting user:", err)
-				// }
-				
-				// msgText := fmt.Sprintf("Cześć, %s %s! %s \n", user.FirstName, user.LastName, responseText)
-
-				// err = messenger.SendMessage(webhookData.Sender.ID, msgText, accessToken)
-				err = messenger.SendPostback(webhookData.Sender.ID, accessToken)
-				if err != nil {
-					fmt.Println("Error sending message:", err)
-				}
-
-				// err = database.SaveOrder(webhookData, webhookData.Message.Text)
-				// if err != nil {
-				// 	fmt.Println("Error saving order to firestore:", err)
-				// }
-
-			} else if webhookData.Postback != nil {
-				msgText, err := webhookData.HandlePostback()
-				if err != nil {
-					fmt.Println("Error handling postback")
-				}
-
-				err = messenger.SendMessage(webhookData.Sender.ID, msgText, accessToken)
-				if err != nil {
-					fmt.Println("Error sending message:", err)
-				}
-			}
-
-		}
-
-		s, _ := json.MarshalIndent(entries, "", "    ")
-		fmt.Printf("webhookEvents: %s\n", string(s))
-
-		w.WriteHeader(http.StatusOK)
+func postbackReceived(ctx context.Context, msng *messenger.Messenger, userID int64, p messenger.FacebookPostback) {
+	if p.Payload == "postback" {
+		// user just clicked Ok button from previouse example, lets just send him a message
+		msng.SendTextMessage(userID, "Ok, I'm always online, chat with me anytime :)")
 	}
 }
